fc626f0387f66bf38be5c2478f3e4d03141b2aad
LaurisGi <laurynas.gipas@gmail.com>
2023-02-01 17:48:08 +0200

    register added to BE

--- app/app.js
+++ app/app.js
@@ -2,9 +2,12 @@ const mysql = require('mysql2');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const { restart } = require('nodemon');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const mysqlConfig = {
     host: process.env.MYSQL_HOST,
@@ -16,9 +19,40 @@ const mysqlConfig = {
   
   const connection = mysql.createConnection(mysqlConfig);
 
-  connection.execute('SELECT * FROM users', (err, expenses) => {
-    console.log(expenses);
+
+  app.get('/login', (req, res) => {
+    const { id } = req.params;
+  connection.execute('SELECT * FROM users', (err, users) => {
+    res.send(users);
    });
+  });
+
+  app.post('/register', (req, res) =>  {
+    const { name, surname, email, password } = req.body;
+    connection.execute(
+      'SELECT email FROM users WHERE email = ?',
+      [email],
+      (err, results) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send({ message: 'There was a problem registering, please try again later' });
+        } else if (results.length > 0) {
+          res.status(400).send({ message: 'Email already exists' });
+        } else {
+          const hashedPassword = bcrypt.hashSync(password, 12);
+          connection.execute(
+            'INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)',
+            [name, surname, email, hashedPassword], (err, result) => {
+              if (err) {
+                console.error(err);
+                res.status(500).send({ message: 'There was a problem registering, please try again later' });
+              } else {
+                res.status(200).send({message: 'OK'});
+              }
+            });
+        }
+      });
+});
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Express server running on PORT:${PORT}`));
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Input } from '../components/Input/Input';
+import { Button } from '../components/Button/Button';
 
 export const Register = () => {
-  return (
-    <div>register</div>
-  )
-}
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleRegister = (e) => {
+      e.preventDefault();
+      fetch(`${process.env.REACT_APP_API_URL}/register`, {
+          method : 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name,
+            surname,
+            email,
+            password
+        })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(name, surname, email, password)
+          navigate('/login')})
+  }
+  
+    return (
+        <>
+        <form onSubmit={handleRegister}>
+          <Input
+          placeholder='Name'
+          onChange={(e)=> setName(e.target.value)}
+          value={name}
+          />
+          <Input
+          placeholder='Surname'
+          onChange={(e)=> setSurname(e.target.value)}
+          value={surname}
+          />
+          <Input
+          placeholder='Email'
+          type="email"
+          onChange={(e)=> setEmail(e.target.value)}
+          value={email}
+          />
+          <Input
+          placeholder='Password' 
+          type="password"
+          onChange={(e)=> setPassword(e.target.value)}
+          value={password}
+          />
+          <Button>Register</Button>
+        </form>
+        <Link to="/login">Login</Link>
+        </>
+      )
+    }
